306dfcdfa65886cf50da1e00a737040a5432787a
jaberbel <33329157+jaberbel@users.noreply.github.com>
2018-05-10 12:18:40 +0100

    Update app.py

--- flaskapp_orginal/app.py
+++ flaskapp_orginal/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request, jsonify
 from data import Articles
+import pandas as pd
 
 app = Flask (__name__)
 
@@ -20,6 +21,25 @@ def about ():
 def articles ():
 	return render_template ('articles.html', articles= Articles)
 
+@app.route("/upload", methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        print(request.files['file'])
+        f = request.files['file']
+        data_xls = pd.read_excel(f)
+        return data_xls.to_html()
+    return '''
+    <!doctype html>
+    <title>Upload an excel file</title>
+    <h1>Excel file upload (csv, tsv, csvz, tsvz only)</h1>
+    <form action="" method=post enctype=multipart/form-data>
+    <p><input type=file name=file><input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route("/export", methods=['GET'])
+def export_records():
+    return 
 
 if __name__ == '__main__' :
 	app.run(debug= True)
