e8caab778e01d84dd9eda94deeddd49bffeca8e6
Houcemeddine Othman <houcemoo@gmail.com>
2018-05-10 13:42:04 +0100

    git commit -m "flask upload csv and save to folder data"

--- flaskapp_orginal/app.py
+++ flaskapp_orginal/app.py
@@ -25,17 +25,13 @@ def articles ():
 def upload_file():
     if request.method == 'POST':
         print(request.files['file'])
-        f = request.files['file']
-        data_xls = pd.read_excel(f)
-        return data_xls.to_html()
-    return '''
-    <!doctype html>
-    <title>Upload an excel file</title>
-    <h1>Excel file upload (csv, tsv, csvz, tsvz only)</h1>
-    <form action="" method=post enctype=multipart/form-data>
-    <p><input type=file name=file><input type=submit value=Upload>
-    </form>
-    '''
+        f = request.files['file']        
+        data_csv = pd.read_csv(f)
+        return data_csv.to_html(), data_csv.to_csv("./data/data.csv")
+    return render_template('upload.html')
+
+
+    
 
 @app.route("/export", methods=['GET'])
 def export_records():
