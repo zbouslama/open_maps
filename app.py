1e4918d37da417cd26d5f8e10acc6868b7a805f0
zbouslama <zied.bouslama@pasteur.rns.tn>
2018-05-10 13:03:25 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template,request, jsonify
+from data import Articles
+import pandas as pd
+
+app = Flask (__name__)
+
+Articles= Articles()
+
+
+
+@app.route('/')
+
+def index():
+		return render_template ('home.html')
+
+@app.route('/about')
+def about ():
+	return render_template ('about.html')
+
+@app.route('/articles')
+def articles ():
+	return render_template ('articles.html', articles= Articles)
+
+@app.route("/upload", methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        print(request.files['file'])
+        f = request.files['file']
+        data_xls = pd.read_excel(f)
+        return data_xls.to_html()
+    return render_template ('upload.html')
+
+@app.route("/export", methods=['GET'])
+def export_records():
+    return 
+
+if __name__ == '__main__' :
+	app.run(debug= True)
---
+++ readme.md
@@ -0,0 +1,3 @@
+
+#this is the first flask app I'm creating.
+I am follwing a [youtube video] (https://www.youtube.com/watch?v=zRwy8gtgJ1A)
