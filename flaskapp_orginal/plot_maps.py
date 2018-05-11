d707fe794dc1654d867de43d8afd009ef22fa128
Houcemeddine Othman <houcemoo@gmail.com>
2018-05-11 14:35:24 +0100

    add automatic zooming

--- flaskapp_orginal/plot_maps.py
+++ flaskapp_orginal/plot_maps.py
@@ -1,25 +1,41 @@
 import folium
 import numpy as np 
 import pandas as pd 
+from scipy.spatial import distance
 
 data=pd.read_csv("/home/houcemeddine/Desktop/test_geo_1.csv", sep=';', header=None)
-
 
 maxLon=np.max( data[3] )
 minLon=np.min ( data[3] )
 maxLat=np.max( data[4] )
 minLat=np.min ( data[4] )
 
-def plot_point():
+def centerpoint(Lon, Lat):
+    maxLon = np.max( Lon )
+    minLon = np.min ( Lon )
+    maxLat = np.max( Lat )
+    minLat = np.min ( Lat )
+    minmax= [minLat, maxLon]
+    maxmin= [maxLat, minLon ]
+    maxmax= [maxLat, maxLon ]
+    
+    disLatHalf = ( distance.euclidean(maxmax,maxmin) )/2
+    disLongHalf = ( distance.euclidean(maxmax,minmax) )/2
+    centerLat= maxLat - disLatHalf
+    centerLon= maxLon - disLongHalf
+    return centerLat,  centerLon
+
+
+def plot_point(longitude, latitude):
+    a, b = centerpoint( longitude, latitude )
     m = folium.Map(
-        location=[maxLon , minLat],
-        tiles='Stamen Toner',
-        zoom_start=13
+        location=[a , b],
+        tiles='stamentoner',
+        zoom_start=6
     )
-    for lon, lat in zip(data[3], data[4]):
+    for lon, lat in zip(longitude , latitude ):
         fcirc=folium.Circle(radius=3000, location=[lat, lon], popup='The Waterfront', color='crimson', fill=False )
         fcirc.add_to(m)
     return m
         
-
-m=plot_point()
+m=plot_point(data[3], data[4])
